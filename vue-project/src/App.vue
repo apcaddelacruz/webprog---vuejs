c48b711a1d0b673cfda16308b01bbda8cf309f19
apcaddelacruz <addelacruz2@student.apc.edu.ph>
2025-01-26 21:00:54 +0800

    updated

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,8 +1,9 @@
 <template>
-  <h1>Food</h1> <br>
+    <h1>Food</h1> <br>
   <food-item/> <br>
+  <food-item2/> <br>
   <food-item/> <br>
-  <food-item/> <br>
+  <food-item2/> <br>
 </template>
 
 <script></script>
