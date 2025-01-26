9dc36021fa3ca3b28432ababa91a8914882ae116
apcaddelacruz <addelacruz2@student.apc.edu.ph>
2025-01-26 20:32:47 +0800

    updated app.vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -7,5 +7,13 @@
 
 <script></script>
 
-<style></style>
+<style>
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+</style>
  
