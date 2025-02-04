a9709dfb9bee69394e7f7b6ea94eff8166b2af8a
apcaddelacruz <addelacruz2@student.apc.edu.ph>
2025-02-04 13:03:46 +0800

    updated app.vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,4 +1,24 @@
+<script setup>
+  import { ref, onMounted } from 'vue'
+  import { supabase } from './lib/supabaseClient'
+
+  const instruments = ref([])
+
+  async function getInstruments() {
+    const { data } = await supabase.from('instruments').select()
+    instruments.value = data
+  }
+
+  onMounted(() => {
+    getInstruments()
+  })
+  </script>
+
 <template>
+      <ul>
+      <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+    </ul>
+
   <h1>Food</h1> <br>
   <personal-profile/> <br>
   <food-item/> <br>
@@ -7,7 +27,6 @@
   <food-item2/> <br>
 </template>
 
-<script></script>
 
 <style></style>
  
